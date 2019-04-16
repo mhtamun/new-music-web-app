0355bc81b629d997744ec2c172b0d935215f2832
Maruf Hossain <maruf.hossaint@dsinnovators.com>
2019-04-16 17:14:32 +0600

    Updates

---
+++ precache-manifest.7bf621059c766dd1611f5555ce063355.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1a5307240168a83e8e4",
+    "url": "/new-music-web-app/static/js/runtime~main.d951a773.js"
+  },
+  {
+    "revision": "fbcd94b80e10f167d63c",
+    "url": "/new-music-web-app/static/js/main.fba29d28.chunk.js"
+  },
+  {
+    "revision": "9c7ef8d21c3687d722b2",
+    "url": "/new-music-web-app/static/js/2.c4202468.chunk.js"
+  },
+  {
+    "revision": "0fd6d0b0dd37c77a160f826fb9002e35",
+    "url": "/new-music-web-app/index.html"
+  }
+];
